dca58bc691312af47b2dab262b33acb57f9ed508
maxlestage <maxlestage@icloud.com>
2021-10-22 21:44:33 +0200

    login it's OK

--- backend/app/controllers/users.controller.js
+++ backend/app/controllers/users.controller.js
@@ -5,53 +5,48 @@ const jwt = require('jsonwebtoken');
 // Fonction pour créer un utilisateur avec un mot de passe chiffré|Hashé à l'aide de bcrypt.
 exports.userSignup = async (req, res) => {
     // Ma fonction de création de compte
-    try {
-        let user = await User.findOne({ where: { mail: req.body.mail } });
-        if (user !== null) {
-            return res.status(401).json({
-                err: 'Un utilisateur utilise déjà cette adresse mail. ',
-            });
-        }
-        if (user === null) {
-            const hash = await bcrypt.hash('req.body.password', 10);
-            user = await User.create({
-                lastName: req.body.lastName, // req.body.lastName 'Ben'
-                firstName: req.body.firstName, // req.body.firstName 'LESTAGE'
-                mail: req.body.mail, // req.body.mail 'max@max.com'
-                password: hash,
-                birthday: new Date('December 17, 1995'), // req.body.birthday
-                profilePicture: 0, // req.body.profilePicture
-                isEnable: 0, // req.body.isEnable
-            });
-            console.log('je suis ici');
-            return res.status(201).json({ message: 'Utilisateur créé' });
-        }
-    } catch ({ error }) {}
+    let user = await User.findOne({ where: { mail: req.body.mail } });
+    if (user !== null) {
+        return res.status(401).json({
+            err: 'Un utilisateur utilise déjà cette adresse mail. ',
+        });
+    }
+    if (user === null) {
+        const hash = await bcrypt.hash(req.body.password, 10);
+        user = await User.create({
+            lastName: req.body.lastName, // req.body.lastName 'Ben'
+            firstName: req.body.firstName, // req.body.firstName 'LESTAGE'
+            mail: req.body.mail, // req.body.mail 'max@max.com'
+            password: hash,
+            birthday: new Date('December 17, 1995'), // req.body.birthday
+            profilePicture: 0, // req.body.profilePicture
+            isEnable: 0, // req.body.isEnable
+        });
+        console.log('je suis ici');
+        return res.status(201).json({ message: 'Utilisateur créé' });
+    }
 };
 
 exports.userLogin = async (req, res) => {
     // Ma fonction de connexion
-    try {
-        let user = await User.findOne({ where: { mail: req.body.mail } });
-        if (user) {
-            const hash = req.body.password;
-            console.log(hash);
-            console.log(user.password);
-            let valid = bcrypt.compareSync(hash, user.password);
-            // console.log(!valid);
-            // console.log(valid);
-            if (!valid) {
-                console.log('err');
-                return res
-                    .status(401)
-                    .json({ error: 'Mot de passe incorrect !' });
-            }
-            console.log('bon');
-            return res.status(200).json({
-                token: jwt.sign({ userId: user.id }, 'RANDOM_TOKEN_SECRET', {
-                    expiresIn: '24h',
-                }),
-            });
+
+    let user = await User.findOne({ where: { mail: req.body.mail } });
+    if (user) {
+        const hash = req.body.password;
+        // console.log(hash);
+        // console.log(user.password);
+        let valid = await bcrypt.compare(hash, user.password);
+        // console.log(!valid);
+        // console.log(valid);
+        if (!valid) {
+            console.log('err');
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
         }
-    } catch ({ error }) {}
+        console.log('bon');
+        return res.status(200).json({
+            token: jwt.sign({ userId: user.id }, 'RANDOM_TOKEN_SECRET', {
+                expiresIn: '24h',
+            }),
+        });
+    }
 };
