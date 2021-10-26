e366d26f6237d09184d13432e65b99f585650372
maxlestage <maxlestage@icloud.com>
2021-10-26 17:25:26 +0200

    Ajout d'une fonctionnalité pour afficher tous les commentaires d'un post.

--- backend/app/controllers/comments.controller.js
+++ backend/app/controllers/comments.controller.js
@@ -1,7 +1,7 @@
 const { Comment, Post, User } = require('../models/index.model.js');
 
 exports.create = async (req, res) => {
-    const user = await User.findByPk(req.body.id);
+    const user = res.locals.user;
     const post = await Post.findByPk(req.params.postid);
 
     if (user === null) {
@@ -19,3 +19,24 @@ exports.create = async (req, res) => {
     });
     return res.status(201).json(comment.toJSON());
 };
+
+exports.findAllByPost = async (req, res) => {
+    // recherche à partir de l'id => renvoie tout le post
+    const post = await Post.findByPk(req.params.postid);
+    if (post === null) {
+        return res.status(404).json({ message: 'Aucun post disponible.' });
+    }
+
+    const comments = await post.getComments();
+    return res.status(200).json(comments);
+
+    /* // const comments = await post.getComments()
+        identique => 
+    const comments = await Comment.findAll({
+        where: { postId: post.id },
+    }); */
+};
+
+// select postId
+// from comment
+// where comment.postId = post.id
