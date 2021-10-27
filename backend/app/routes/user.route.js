3f7a884738b51ce922501f848af94dc832de767f
maxlestage <maxlestage@icloud.com>
2021-10-27 11:05:08 +0200

    Ajout de multer

--- backend/app/routes/user.route.js
+++ backend/app/routes/user.route.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+// Middleware qui gère l'upload sur notre app.
+const multer = require('multer');
+const upload = require('../middleware/multer_config');
+// const destination = multer({ dest: 'images/' });
+
+const sessionAuth = require('../middleware/session.js');
+
 // Import du controller user pour gérer les actions.
 const userCtrl = require('../controllers/users.controller.js');
 
 // /api/auth/routeActionController.
-router.post('/signup', userCtrl.userSignup);
+router.post('/signup', upload.single('profilePicture'), userCtrl.userSignup);
 router.post('/login', userCtrl.userLogin);
+router.put(
+    '/edit',
+    sessionAuth,
+    upload.single('profilePicture'),
+    userCtrl.userEdit
+);
 
 // Permet d'utiliser user.js du répertoire routes.dans app.js à la base du projet.
 module.exports = router;
