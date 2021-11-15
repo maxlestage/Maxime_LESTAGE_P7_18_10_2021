f2c80c0ef4ffdf7f2f0c9ddae35ef5ce6544099f
maxlestage <maxlestage@icloud.com>
2021-11-15 09:23:51 +0100

    Ajout de class bootstrap pour le style.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,8 +3,8 @@ import "../styles/footer.css";
 function Footer() {
   return (
     <>
-      <footer className="footer bg-light text-center text-lg-start">
-        <div className="text-center p-3">
+      <footer className="footer bg-light text-center text-lg-start footer-auto">
+        <div className="container text-center p-3">
           © 2021 Copyright:
           <a className="text-dark" href="/">
             Groupomania
