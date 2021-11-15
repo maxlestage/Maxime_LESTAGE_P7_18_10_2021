94d995ca4278fb7f7628df12566d61e073fec5da
maxlestage <maxlestage@icloud.com>
2021-11-15 09:28:58 +0100

    Template formumaire

--- frontend/src/components/InputPost.js
+++ frontend/src/components/InputPost.js
@@ -1,46 +1,62 @@
+import "../styles/card.css";
 function Inputpost() {
   return (
-    <div className="post-card w-75">
-      <form>
-        <p>Vous pouvez partager vos idées ici :</p>
-        <div className="card">
-          <div className="form-group w-75 m-auto">
-            <label htmlFor="exampleInputEmail1">
-              Comment pouvons-nous vous aider ?
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Titre:"
-              aria-describedby="emailHelp"
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              Le choix d'un bon titre est important.
-            </small>
-          </div>
-          <div className="card-body">
-            <div className="form-group w-75 m-auto">
-              <label htmlFor="exampleInputEmail1">
-                Ici, vous pouvez partager vos idées:
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                placeholder="Corps du message:"
-                aria-describedby="emailHelp"
-              />
-              <small id="emailHelp" className="form-text text-muted">
-                Dites-nous ce qui vous passe par la tête.
-              </small>
+    <div className="card-container-input-post ">
+      <div className="container-fluid mt-100">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="post-card">
+                <form className="form form-input-post">
+                  <h2>
+                    Vous pouvez partager vos idées avec l'équipe{" "}
+                    <span className="span span-group">Groupomania</span>
+                  </h2>
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="form-group  m-auto">
+                        <label htmlFor="TitlePost">
+                          Comment pouvons-nous vous aider ?
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="TitlePost"
+                          placeholder="Titre:"
+                          aria-describedby="TitlePost"
+                        />
+                        <small id="TitlePost" className="form-text text-muted">
+                          Le choix d'un bon titre est important.
+                        </small>
+                      </div>
+
+                      <div className="form-group m-auto">
+                        <label htmlFor="ContentPost">
+                          Dites-nous ce qui vous passe par la tête.
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="ContentPost"
+                          placeholder="Corps du message:"
+                          aria-describedby="ContentPost"
+                        />
+                      </div>
+                    </div>
+                    <div className="card-footer">
+                      <div className="btn">
+                        <a href="#" className="btn btn-post btn-outline-dark">
+                          Partager
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
