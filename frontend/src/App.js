294098dbefcfe86549542db2020049e787976e7e
maxlestage <maxlestage@icloud.com>
2021-11-16 20:43:46 +0100

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,29 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Profile from "./components/Profile";
+import { useState, useEffect } from "react";
+// import Profile from "./components/Profile";
+
+import axios from "axios";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  function userIsConnected() {
+    return axios({ method: "GET", url: `http://localhost:3000/api/auth/user` });
+  }
+
+  useEffect(() => {
+    userIsConnected().then((response) => {
+      setCurrentUser(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Profile />
+      <Header currentUser={currentUser} onLogin={setCurrentUser} />
+      <Home currentUser={currentUser} />
+      {/* <Profile /> */}
 
       <Footer />
     </div>
