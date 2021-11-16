14e43e510352e187ef9a14bc2147734d1d987fe5
maxlestage <maxlestage@icloud.com>
2021-11-16 20:42:47 +0100

    Ajout d'attributes exclude  pour masquer les données

--- backend/app/middleware/session.js
+++ backend/app/middleware/session.js
@@ -3,10 +3,14 @@ const { User } = require('../models/index.model');
 module.exports = async (req, res, next) => {
     // Si dans la session du client act' on a pas la valeur de l'id => on est pas auth'
     if (req.session.userId === undefined) {
-        return res.status(401).json({ message: "Vous n'êtes pas conecté" });
+        return res.status(401).json({ message: "Vous n'êtes pas connecté" });
     } else {
         // Si la personne est connectée :
-        res.locals.user = await User.findByPk(req.session.userId);
+        res.locals.user = await User.findByPk(req.session.userId, {
+            attributes: {
+                exclude: ['password', 'createdAt', 'updatedAt', 'isEnable'],
+            },
+        });
         next();
     }
 };
