5444709dc75e4bcb08ba4ba060137ae2f76be00d
maxlestage <maxlestage@icloud.com>
2021-11-16 20:47:53 +0100

    Delete le interceptor et mise en majuscule des Methodes HTTP

--- frontend/src/service/login.js
+++ frontend/src/service/login.js
@@ -1,25 +1,21 @@
 import axios from "axios";
+axios.defaults.withCredentials = true;
+function userSignUp() {
+  // axios.defaults.withCredentials = true;
+  return axios({
+    method: "POST",
+    // headers: {
+    //   "Content-Type": "application/x-www-form-urlencoded",
+    // },
+    url: "http://localhost:3000/api/auth/signup",
+    data: FormData,
+  });
+}
 
 function userLogin() {
-  axios.interceptors.response.use(
-    function (response) {
-      // Do something with response data
-      console.log(response);
-      return response;
-    },
-    function (error) {
-      console.log({ error });
-      // Do something with response error
-      if (error.status === 401) {
-        console.log("t'es pas conecté");
-      }
-      return Promise.reject(error);
-    }
-  );
-  console.log("test");
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true
   return axios({
-    method: "post",
+    method: "POST",
     // headers: {
     //   "Content-Type": "application/x-www-form-urlencoded",
     // },
@@ -28,13 +24,19 @@ function userLogin() {
       mail: "max@test.com",
       password: "jefaisuntest35",
     },
-  });
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return { message: err };
+    });
 }
 function userLogout() {
   console.log("Logout");
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true;
   return axios({
-    method: "delete",
+    method: "DELETE",
     // headers: {
     //   "Content-Type": "application/x-www-form-urlencoded",
     // },
@@ -42,4 +44,4 @@ function userLogout() {
   });
 }
 
-export { userLogin, userLogout };
+export { userLogin, userLogout, userSignUp };
