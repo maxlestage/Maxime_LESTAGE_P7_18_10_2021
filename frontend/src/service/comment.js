db9ad0fbab403ca87613e49e4adf9fcbdba45538
maxlestage <maxlestage@icloud.com>
2021-11-16 20:48:25 +0100

    Mise en majuscule des methodes HTTP

--- frontend/src/service/comment.js
+++ frontend/src/service/comment.js
@@ -1,25 +1,56 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
+
 function getAllComment(postId) {
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true;
   // postId = 2;
   return axios({
-    method: "get",
+    method: "GET",
     url: `http://localhost:3000/api/posts/${postId}/comments`,
   });
   // }).then((response) => console.log(response.data));
 }
 
 function postComment(postId, userContent) {
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true;
   // postId = 2;
   return axios({
-    method: "post",
+    method: "POST",
     url: `http://localhost:3000/api/posts/${postId}/comments`,
     data: { content: userContent },
   });
   // .then((response) => console.log(response.data))
   // .catch();
+}
+
+function editCommentByUser(postId) {
+  // axios.defaults.withCredentials = true;
+  // postId = 2;
+  return axios({
+    method: "PUT",
+    url: `http://localhost:3000/api/posts/${postId}/comments`,
+    data: {},
+  });
+}
+
+function deleteCommentByUser(postId) {
+  // axios.defaults.withCredentials = true;
+  // postId = 2;
+  return axios({
+    method: "DELETE",
+    url: `http://localhost:3000/api/posts/${postId}/comments`,
+  });
+}
+
+function deleteCommentByAdmin(postId) {
+  // axios.defaults.withCredentials = true;
+  // postId = 2;
+  return axios({
+    method: "PUT",
+    url: `http://localhost:3000/api/posts/${postId}/comments`,
+    data: {},
+  });
 }
 
 export { getAllComment, postComment };
--- frontend/src/service/post.js
+++ frontend/src/service/post.js
@@ -1,21 +1,63 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
 // get all posts on the platform
 function getAllPost() {
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true;
   return axios({
-    method: "get",
+    method: "GET",
     url: "http://localhost:3000/api/posts",
   });
 }
 
-// get all posts from user logged in
-function getAllPostByUser(userId) {
-  axios.defaults.withCredentials = true;
-  // return axios({
-  //   method: "get",
-  //   url: `localhost:3000/api/auth/${1}`,
-  // });
+// // get all posts from user logged in
+// function getAllPostByUser(userId) {
+//   // axios.defaults.withCredentials = true;
+//   // return axios({
+//   //   method: "get",
+//   //   url: `localhost:3000/api/auth/${1}`,
+//   // });
+// }
+
+function postByUser(formData) {
+  // axios.defaults.withCredentials = true;
+  console.log(formData);
+  return axios({
+    method: "POST",
+    url: "http://localhost:3000/api/posts/",
+    data: formData,
+  });
 }
 
-export { getAllPost, getAllPostByUser };
+function editPostByUser(id) {
+  // axios.defaults.withCredentials = true;
+  return axios({
+    method: "PUT",
+    url: "http://localhost:3000/api/posts",
+  });
+}
+
+function deletePostByUser() {
+  // axios.defaults.withCredentials = true;
+  return axios({
+    method: "DELETE",
+    url: "http://localhost:3000/api/posts",
+  });
+}
+
+function deletePostByAdmin() {
+  // axios.defaults.withCredentials = true;
+  return axios({
+    method: "DELETE",
+    url: "http://localhost:3000/api/posts",
+  });
+}
+
+export {
+  getAllPost,
+  // getAllPostByUser,
+  postByUser,
+  editPostByUser,
+  deletePostByUser,
+  deletePostByAdmin,
+};
