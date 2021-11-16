f828926182aae3c018ba93250a2f23a745720675
maxlestage <maxlestage@icloud.com>
2021-11-16 20:45:50 +0100

    Création du signin si pas connecté

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,20 @@
-import Card from "./Card";
+import Cards from "./Cards";
 // import Login from "./Login";
 // import Comment from "./Comment";
 // import UserPost from "./UserPost";
 // import Inputpost from "./InputPost";
 // import Toasts from "./Toasts";
+import SignIn from "./SignIn";
 
-function Home() {
+function Home({ currentUser }) {
+  if (!currentUser) {
+    return (
+      <div className="loggin_off">
+        <SignIn />
+      </div>
+    );
+  }
+
   return (
     <>
       {/* <Login choice={choice} onClick={() => setChoice(choice + 1)} /> */}
@@ -13,7 +22,7 @@ function Home() {
       {/* <Inputpost /> */}
       {/* <UserPost /> */}
 
-      <Card />
+      <Cards />
     </>
   );
 }
